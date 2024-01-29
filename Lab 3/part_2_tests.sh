faf55befe2788b133f892d054ddff4278e7b08a7
masonjamie <69094064+masonjamie@users.noreply.github.com>
2024-01-28 21:41:08 -0800

    Update part_2_tests.sh

--- Lab 3/part_2_tests.sh
+++ Lab 3/part_2_tests.sh
@@ -11,11 +11,11 @@ echo "--------------------------------------------------------------------------
     fi
 
     # Check if srand() is called more than once, excluding single-line comments
-    srand_count=$(grep -v "^\s*//" ./source/main.cpp | grep -c "srand"
-    if [ "$srand_count" -gt 1 ]; then
-        echo "❌ FAILED: srand() appears in code more than once. Ensure srand() appears only once in your code."
-        exit 1  # Exit this script with an error status
-    fi
+srand_count=$(grep -v "^\s*//" ./source/main.cpp | grep -c "srand(")
+if [ "$srand_count" -gt 1 ]; then
+    echo "❌ FAILED: srand() appears in code more than once. Ensure srand() appears only once in your code."
+    exit 1  # Exit this script with an error status
+fi
 
     
 # Function to replace seed, compile, run the game, and check the results
